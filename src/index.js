16507b4034128653461830052d3e62067472df10
Laisa Pereira <laisadsilva.pereira@gmail.com>
2024-09-25 14:32:08 -0300

    ajustando filtro

--- src/index.js
+++ src/index.js
@@ -1,6 +1,84 @@
 
+const toothProducts = [
+  {
+    name: "Pasta de dente",
+    type: "Pasta de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_toothpaste.svg",
+    alt: "./assets/images/img_toothpaste.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+
+  },
+  {
+    name: "Pasta de dente",
+    type: "Pasta de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_toothpaste.svg",
+    alt: "./assets/images/img_toothpaste.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+
+  },
+  {
+    name: "Pasta de dente",
+    type: "Pasta de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_toothpaste.svg",
+    alt: "./assets/images/img_toothpaste.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+
+  },
+  {
+    name: "Pasta de dente",
+    type: "Pasta de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_toothpaste.svg",
+    alt: "./assets/images/img_toothpaste.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+
+  },
+  {
+    name: "Escova de dente",
+    type: "Escova de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_tooth.svg",
+    alt: "./assets/images/img_tooth.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+
+  },
+  {
+    name: "Escova de dente",
+    type: "Escova de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_tooth.svg",
+    alt: "./assets/images/img_tooth.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+  },
+  {
+    name: "Escova de dente",
+    type: "Escova de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_tooth.svg",
+    alt: "./assets/images/img_tooth.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+
+  },
+  {
+    name: "Escova de dente",
+    type: "Escova de dente",
+    price: "R$ 70,90",
+    img_url: "./assets/images/img_tooth.svg",
+    alt: "./assets/images/img_tooth.svg",
+    fake_rating: "./assets/icons/ic_rate.svg"
+
+  },
+ ]
+
 const filterButtons = document.querySelectorAll(".section-filter button")
-/* const cards = document.querySelectorAll('.section-type-products')  */
+const cards = document.querySelectorAll('.product-card')
+
+// rever isso aqui
+const productTypes = toothProducts.map(({type}) => type)
+const uniqueTypes = [...new Set(productTypes)]
 
 filterButtons.forEach(button => {
   button.addEventListener('click', filterCards)
@@ -9,126 +87,48 @@ filterButtons.forEach(button => {
 function filterCards(e) {
  const typeButton = e.target.value
 
+ console.log(typeButton)
+
  const cardsTitles = document.querySelectorAll('.section-type-products h4')
  cardsTitles.forEach(
   cardTitle => {
-    const valueCardTitle = cardTitle.innerText
-    console.log(valueCardTitle)
+  
+
+    const valueCardTitle = cardTitle.innerText 
     const card = cardTitle.parentElement
-    console.log(card)
-    if (valueCardTitle === typeButton) {
-      card.classList.remove('hide-component')
-      card.classList.add('show-component')
-    } else if (typeButton === 'Todos') {
-      card.classList.add('show-component')
-      card.classList.remove('hide-component') 
+
+    if (!uniqueTypes.includes(typeButton) && typeButton !== 'Todos') {
+      renderingErrorSection(typeButton) 
+      console.log("deu b.o" + valueCardTitle)
+ 
     } else {
-      card.classList.remove('show-component')
-      card.classList.add('hide-component')
+      valueCardTitle === typeButton || typeButton === 'Todos' 
+      ? card?.classList.remove('hide-component') 
+      : card?.classList.add('hide-component') 
+      console.log("deu bom" + valueCardTitle)
 
-    } 
-  })
- }
-
-const ToothPasteproducts = [
-  {
-    name: "Pasta de dente",
-    type: "Pasta de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_toothpaste.svg",
-    alt: "./assets/images/img_toothpaste.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
-
-  },
-  {
-    name: "Pasta de dente",
-    type: "Pasta de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_toothpaste.svg",
-    alt: "./assets/images/img_toothpaste.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
-
-  },
-  {
-    name: "Pasta de dente",
-    type: "Pasta de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_toothpaste.svg",
-    alt: "./assets/images/img_toothpaste.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
-
-  },
-  {
-    name: "Pasta de dente",
-    type: "Pasta de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_toothpaste.svg",
-    alt: "./assets/images/img_toothpaste.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
-
-  },
- ]
-
-
- document.querySelector('.tooth-paste-products').innerHTML = ToothPasteproducts.map(product => {
-  return `
-
+    }
+ 
+    document.querySelector('.section-title').textContent = `${typeButton}`
         
-        <div class="product-card">
-          <img src="${product.img_url}" alt="${product.alt}" />
-          <h4>${product.name}</h4>
-          <p>${product.price}</p>
-          <img src="${product.fake_rating}" alt="fake rating" />
-          <button>Adicionar ao carrinho</button>
-        </div>
+  },
+
+)
+}
+
+function renderingErrorSection(typeButton) {
+  document.querySelector('.section-type-products').innerHTML = `
+    <div class="error-section">
+      <h3>${typeButton}</h3>
+      <h2>Ops! Parece que não temos esse produto por aqui</h2>
+    </div>
   `
- }).join('')
+}
 
- 
- 
- const ToothBrushproducts = [
-  {
-    name: "Escova de dente",
-    type: "Escova de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_tooth.svg",
-    alt: "./assets/images/img_tooth.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
 
-  },
-  {
-    name: "Escova de dente",
-    type: "Escova de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_tooth.svg",
-    alt: "./assets/images/img_tooth.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
-
-  },
-  {
-    name: "Escova de dente",
-    type: "Escova de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_tooth.svg",
-    alt: "./assets/images/img_tooth.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
-
-  },
-  {
-    name: "Escova de dente",
-    type: "Escova de dente",
-    price: "R$ 70,90",
-    img_url: "./assets/images/img_tooth.svg",
-    alt: "./assets/images/img_tooth.svg",
-    fake_rating: "./assets/icons/ic_rate.svg"
-
-  },
- ]
-
- 
- document.querySelector('.tooth-brush-products').innerHTML = ToothBrushproducts.map(product => {
+document.querySelector('.tooth-products').innerHTML = toothProducts.map(product => {
   return `
-
+      
         <div class="product-card">
           <img src="${product.img_url}" alt="${product.alt}" />
           <h4>${product.name}</h4>
@@ -139,4 +139,9 @@ const ToothPasteproducts = [
   `
  }).join('')
 
+
+
+
+ 
+ 
 
